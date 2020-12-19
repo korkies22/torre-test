dd9c96e51379cdb6d7af82091eb957df6ae70f86
korkies22 <cm.sarmiento10@uniandes.edu.co>
2020-12-19 11:55:05 -0500

    Time to start magic in front

--- front-torre/nuxt.config.js
+++ front-torre/nuxt.config.js
@@ -1,5 +1,3 @@
-import colors from 'vuetify/es5/util/colors'
-
 export default {
   // Disable server-side rendering (https://go.nuxtjs.dev/ssr-mode)
   ssr: false,
@@ -44,8 +42,6 @@ export default {
     '@nuxtjs/eslint-module',
     // https://go.nuxtjs.dev/stylelint
     '@nuxtjs/stylelint-module',
-    // https://go.nuxtjs.dev/vuetify
-    '@nuxtjs/vuetify',
   ],
 
   // Modules (https://go.nuxtjs.dev/config-modules)
@@ -61,26 +57,10 @@ export default {
   axios: {},
 
   // Vuetify module configuration (https://go.nuxtjs.dev/config-vuetify)
-  vuetify: {
-    customVariables: ['~/assets/variables.scss'],
-    theme: {
-      dark: true,
-      themes: {
-        dark: {
-          primary: colors.blue.darken2,
-          accent: colors.grey.darken3,
-          secondary: colors.amber.darken3,
-          info: colors.teal.lighten1,
-          warning: colors.amber.base,
-          error: colors.deepOrange.accent4,
-          success: colors.green.accent3,
-        },
-      },
-    },
-  },
+
   styleResources: {
     // your settings here
-    scss: ['./assets/scss/main.scss'],
+    scss: ['~/assets/scss/main.scss'],
   },
   // Build Configuration (https://go.nuxtjs.dev/config-build)
   build: {},
--- front-torre/pages/login/index.vue
+++ front-torre/pages/login/index.vue
@@ -1,3 +1,13 @@
 <template>
-  <div>Login</div>
+  <div class="loginPage">
+    <div class="loginPage__main"></div>
+  </div>
 </template>
+
+<style lang="scss" scoped>
+.loginPage {
+  &__card {
+    padding: 2rem;
+  }
+}
+</style>
